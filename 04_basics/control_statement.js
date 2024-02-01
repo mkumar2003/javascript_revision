91fe1fe1c32d4a56ffa2d0812d216cb5ae65f032
mkumar2003 <152767073+mkumar2003@users.noreply.github.com>
2024-02-01 11:45:38 +0000

    if_else in js

---
+++ 04_basics/control_statement.js
@@ -0,0 +1,57 @@
+// if
+const isUserloggedIn = true
+const temperature = 41
+
+// if ( temperature === 40 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+///comparsion operators used
+// <, >, <=, >=, ==, !=, ===(check if datatype is also same along with value  also known as strict check), !==
+
+
+//if has it own block of scope
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+//it cannot be accessed outside of if block
+
+
+//shorthand method to write if statement
+//if (balance > 500) console.log("test"),console.log("test2");   //however only one statement looks good
+
+
+//use of else if for multiple conditions..
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+//use of &&(and) / ||(or) operator
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
