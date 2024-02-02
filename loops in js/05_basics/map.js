e3f34b7c84b0d6c29e628292c0e0752782436a46
mkumar2003 <152767073+mkumar2003@users.noreply.github.com>
2024-02-02 06:21:06 +0000

    map in js

---
+++ loops in js/05_basics/map.js
@@ -0,0 +1,13 @@
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumers.map( (num) => { return num + 10})
+//map also returns array but do not perform any filtering it iterates over all elements.
+
+//chaining in maps
+//in chaining array is passed to next chain and then after all operations is returned to newNums
+const newNums = myNumers
+                .map((num) => num * 10 )
+                .map( (num) => num + 1)
+                .filter( (num) => num >= 40)
+
+console.log(newNums);
